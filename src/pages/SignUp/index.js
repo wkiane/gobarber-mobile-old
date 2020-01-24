4239b5df09f2be05e6424f71c8dfd183ba0e028f
willa <wkiane@pop-os.localdomain>
2020-01-23 22:43:05 -0300

    configurando background

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text} from 'react-native';
 
 // import { Container } from './styles';
+import Background from '~/components/Background';
 
 export default function SignUp() {
   return (
-    <View>
+    <Background>
       <Text>sign up</Text>
-    </View>
+    </Background>
   );
 }
