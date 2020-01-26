a13474d01e2187c14bca39dcf93ac1f5681bc5f8
willa <wkiane@pop-os.localdomain>
2020-01-26 00:22:11 -0300

    estilos página signup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import {Text} from 'react-native';
-
-// import { Container } from './styles';
+import {Image} from 'react-native';
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
-export default function SignUp() {
+export default function SignUp({navigation}) {
   return (
     <Background>
-      <Text>sign up</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Acessar conta</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
